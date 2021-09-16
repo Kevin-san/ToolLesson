03d8eab2c61087f87d8aa1ce2b4e24e9f7bb36db
xcKev <xckevin1620@outlook.com>
2021-09-16 20:27:50 +0800

    change spider property length

--- sql/mysql/correct_table/SpiderProperty.sql
+++ sql/mysql/correct_table/SpiderProperty.sql
@@ -4,9 +4,9 @@ create table IF NOT EXISTS `SpiderProperty`( -- 爬虫组件属性表
 	`Id` INT UNSIGNED AUTO_INCREMENT,
 	`ItemId` INT NOT NULL,
 	`OrderId` INT NOT NULL,
-	`PropertyKey` VARCHAR(3000) NOT NULL, -- 网址 作者，章节，图片，简介
-	`PropertyValue` VARCHAR(3000) NOT NULL, 
-	`PropertyBigVal` VARCHAR(12000) NOT NULL,
+	`PropertyKey` VARCHAR(500) NOT NULL, -- 网址 作者，章节，图片，简介
+	`PropertyValue` VARCHAR(500) NOT NULL, 
+	`PropertyBigVal` VARCHAR(20000) NOT NULL,
 	`DeleteFlag` TINYINT NOT NULL DEFAULT 0, -- 删除状态
 	`CreateTime` DATETIME NOT NULL DEFAULT NOW(), --创建时间
 	`UpdateTime` DATETIME NOT NULL DEFAULT NOW(), --更新时间
