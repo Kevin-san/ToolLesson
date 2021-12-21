3daf27ad3906a82f58cc785466bc85e1e88bcffe
xcKev <xckevin1620@outlook.com>
2021-12-21 20:33:45 +0800

    update setup config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup,find_packages
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 requirements=open('requirements.txt').readlines()
 
@@ -11,9 +11,14 @@ setup(
     version=__version__,
     author='xcKev',
     author_email='xckevin1620@outlook.com',
-    url='',
+    url='https://github.com/Kevin-san/CorePdfPage',
     description='core_pdf_page',
     packages= find_packages(exclude=['sql','practices']),
+    package_dir= {'application':'core_pdf_page/application',
+                  'core':'core_pdf_page/core',
+                  'static':'core_pdf_page/static',
+                  'templates':'core_pdf_page/templates'
+                  },
     python_requires='>=3.7.0',
     install_requires=requirements
 )
