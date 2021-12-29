2abe34cdf9ee15730faef38ba7cb6bf139d688a2
xcKev <xckevin1620@outlook.com>
2021-12-29 15:52:51 +0800

    fix some bugs.

--- application/PdfWeb/__init__.py
+++ application/PdfWeb/__init__.py
@@ -1,4 +1,5 @@
 import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
 import alvintools.common_logger as log
 current_log=log.get_log('pdfweb', '/temp', 'pdfweb')
--- application/PdfWeb/manage.py
+++ application/PdfWeb/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-
+import alvintools
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PdfWeb.settings')
@@ -13,7 +13,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(['manage.py','runserver_plus','--cert','server.crt','0.0.0.0:8000'])
+    if alvintools.get_system_name() == "Windows":
+        execute_from_command_line(['manage.py','runserver_plus','--cert','server.crt','0.0.0.0:8000'])
+    else:
+        execute_from_command_line(['manage.py','runserver_plus','0.0.0.0:8000'])
 
 
 if __name__ == '__main__':
