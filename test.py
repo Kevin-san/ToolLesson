c60f3a4dcd81a36b59ee600d46ff0e7b1e3ef876
xckevin1620 <xckevin1620@DESKTOP-3H4CN5T>
2022-02-19 15:27:18 +0800

    remove video download

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 from alvintest import testspider
-from alvinspider import videospider
 
 if __name__ == '__main__':
-    videospider.main()
     testspider.test_groupspider_download_spider_results()
     
